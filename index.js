ed9d06726ce0ce75ba052411e853dbe6b5f87221
FARUK2059 <omarfaruk34552@gmail.com>
2024-07-16 05:46:12 +0600

    MongoDB Daatabase Conected Successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,13 +10,57 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Conection Method
+const uri = `mongodb+srv://${process.env.ENV_READYPAY_USER}:${process.env.ENV_READYPAY_PASSWORD}@cluster0.6e55rfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        // Database name Creat
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
 
 // Main Server Function
 app.get('/', (req, res) => {
-    res.send('Mobile Financial Service is Starte')
+    res.send('Mobile Financial Service is Started')
 })
 
 app.listen(port, () => {
